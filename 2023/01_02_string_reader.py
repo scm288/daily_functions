3af0b354d0bb70d7bd08b4328cd20a377c4ae724
Spencer Merrell <scm288@gmail.com>
2023-01-02 16:13:11 -0700

    tried and tried and TRIED to create a word counter; still trying, gotta work

--- 2023/01_02_string_reader.py
+++ 2023/01_02_string_reader.py
@@ -17,11 +17,27 @@
 
 # It turns out that doc[line][word] is actually doc[line][character]--this format skips words entirely! Which I guess makes some sense; it's easier to count characters (including spaces and punctuation) than it is to quantify the limits of individual words, since those vary so much. Huh. How do I go about parsing words, then? Not sure...
 
+word_list = []
+word_counter = []
+
 with open('2023_data/dc_ch_1.txt', 'r') as reader:
     # Read and print the entire file line by line
-    line = reader.readline()
-    while line != '':  # The EOF char is an empty string
-        print(line, end='')
-        line = reader.readline()
+    # line = reader.readline()
+    # while line != '':  # The EOF char is an empty string
+    #     print(line, end='')
+    #     line = reader.readline()
+    for line in reader:
+        for word in line.split():
+            word_entry = {
+                word: 1
+            }
+            if word_entry not in word_counter:
+                word_counter.append(word_entry)
+            elif word_entry in word_counter:
+                temp_value = word_entry[f'{word}']
+                temp_value += 1
+                word_entry[f'{word}'] = temp_value
+                print(word_entry[f'{word}'])
 
-# This is a sample bit of code from the internet. Wow. That works perfectly. It seems that the function/method I was looking for was readline(), which is called on an opened .txt file.
+
+# This is a sample bit of code from the internet. Wow. That works perfectly. It seems that the function/method I was looking for was readline(), which is called on an opened .txt file. And, on top of that, line.split() breaks it up into individual words! Aha! Exactly what I was looking for! Now to read up on that .split() method...
--- scratchpad.py
+++ scratchpad.py
@@ -1,11 +1,74 @@
-list = [1, 2, 3, 4, 5, 6]
-list_segment = []
+# with open('2023_data/dc_ch_1.txt', 'r') as reader:
+#     # Read and print the entire file line by line
+#     print(type(reader))
+#     # line = reader.readline()
+#     # while line != '':  # The EOF char is an empty string
+#     #     print(line, end='')
+#     #     line = reader.readline()
 
-for item in list:
-    if len(list_segment) < 2:
-        list_segment.append(item)
-    else:
-        list_segment = []
-        list_segment.append(item)
-    if len(list_segment) == 2:
-        print(list_segment)
+word_counter = []
+
+reader = ['this this this is is is a a a sample sample sample list list list']
+
+for line in reader:
+    for word in line.split():
+        if word_counter == []:
+            word_entry = {
+                'word_name': word,
+                'count': 1,
+            }
+            word_counter.append(word_entry)
+        else:
+            for entry in word_counter:
+                if entry['word_name'] == word:
+                    entry['count'] += 1
+                elif entry['word_name'] != word:
+
+                    # if entry['word_name'] == word not in word_counter:
+                    #     print(f"The entry '{entry['word_name']}' is not in the list")
+                    #         else:
+                    #             for entry in word_counter:
+                    #                 if entry['word_name'] == word:
+                    #                     entry['count'] += 1
+                    #                 elif entry['word_name'] != word:
+                    #                     continue
+                    #             if entry['word_name'] == word not in word_counter:
+                    #                 print(f"The entry '{entry['word_name']}' is not in the list")
+                    #                 word_entry = {
+                    #                     'word_name': word,
+                    #                     'count': 1,
+                    #                 }
+                    #                 word_counter.append(word_entry)
+                    # print(word_counter)
+
+                    # word_entry = {
+                    #     'word_name': word,
+                    #     'count': 1
+                    # }
+                    # if
+
+                    # if word_entry not in word_counter:
+                    #     word_counter.append(word_entry)
+                    # elif word_entry in word_counter:
+                    #     temp_value = word_entry[f'{word}']
+                    #     temp_value += 1
+                    #     word_entry[f'{word}'] = temp_value
+                    #     for entry in word_counter:
+                    #         if str(word_entry.keys()) in entry.keys():
+                    #             word_counter[f'{word}'] = word_entry
+                    #     print(word_entry[f'{word}'])
+
+                    # for line in reader:
+                    #     for word in line.split():
+                    #         if word_counter == []:
+                    #             word_entry = {
+                    #                 word: 0
+                    #             }
+                    #             word_counter.append(word_entry)
+                    #         for entry in word_counter:
+                    #             if word not in word_counter.keys():
+                    #                 word_entry = {
+                    #                     word: 1
+                    #                 }
+                    #             elif word in word_counter.keys():
+                    #                 word_counter[f'{word}'] += 1
