5b77340898371ae3f62e7bb6807b496b39469bfc
Spencer Merrell <scm288@gmail.com>
2023-01-02 11:00:45 -0700

    .txt files read in lines, not in words

---
+++ 2023/01_02_string_reader.py
@@ -0,0 +1,6 @@
+doc = []
+with open("2023_data/dc_ch_1.txt") as chapter:
+    for line in chapter:
+        print(line)
+
+# It turns out that .txt files are already organized by line. What can I do with this as a result?
